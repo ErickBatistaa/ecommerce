e9be5396b58feb601e22f0adfe6d02d0ef02f3cf
ErickBatistaa <erick.santos067@gmail.com>
2019-12-05 08:35:38 -0300

    criamos pageadmim e tpl admin

--- index.php
+++ index.php
@@ -4,14 +4,15 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/admin', function() {
     
-	$page = new Page();
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 		
